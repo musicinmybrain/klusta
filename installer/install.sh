04b1c38c45c626645dc3726547b931537ed7570f
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-03-22 19:13:50 +0100

    Bump version

--- installer/install.sh
+++ installer/install.sh
@@ -1,1 +1,5 @@
+OUTPUT_DIR = $HOME/klusta
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b -p $OUTPUT_DIR
+export PATH="$OUTPUT_DIR/bin:$PATH"
 conda env create -n klusta3 --force
