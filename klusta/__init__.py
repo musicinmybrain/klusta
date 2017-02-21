c1909dd6e472fa502213f1ee0b1e7b75e8347a71
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-02-21 13:44:39 +0100

    Bump version

--- klusta/__init__.py
+++ klusta/__init__.py
@@ -38,7 +38,7 @@ def _git_version():
 
 __author__ = 'KwikTeam'
 __email__ = 'cyrille.rossant at gmail.com'
-__version__ = '3.0.15'
+__version__ = '3.0.16'
 __version_git__ = __version__ + _git_version()
 
 
